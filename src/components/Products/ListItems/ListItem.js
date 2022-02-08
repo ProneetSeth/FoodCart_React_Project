2aafb99eaeebf4a57e7fcbf268dc26ee0a89925d
ProneetSeth <proneetofficial111@gmail.com>
2022-02-08 20:15:22 +0530

    Passed updateItemTitle as Props and ceated Button to check

--- src/components/Products/ListItems/ListItem.js
+++ src/components/Products/ListItems/ListItem.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import CartIcon from "../../../assets/icons/add_cart.svg"
 
-const ListItem = ({data}) =>{
+const ListItem = ({data, updateItemTitle}) =>{
     const [counter,setCounter] = useState(0)
 
     const increaseCounterByOne = () =>{
@@ -28,6 +28,7 @@ const ListItem = ({data}) =>{
                         <h3>{data.title}</h3>
                     </div>
                 </div>
+                <button>Update The Title</button>
                 {
                     counter < 1 ?
                     <button className={"card-add"} onClick={increaseCounterByOne}>
