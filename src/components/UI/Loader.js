83301603482c18bc23543c915f01bd66e57ec8ce
ProneetSeth <proneetofficial111@gmail.com>
2022-02-10 20:03:49 +0530

    added Loader and then using React Portals to update it

--- src/components/UI/Loader.js
+++ src/components/UI/Loader.js
@@ -1,5 +1,8 @@
+import ReactDom from "react-dom"
+
 const Loader = () =>{
     return(
+       ReactDom.createPortal(
         <>
         <div className="loader-overlay"></div>
         <div className="loading-dots">
@@ -8,7 +11,9 @@ const Loader = () =>{
             <div className="loading-dots--dot"></div>
             <div className="loading-dots--dot"></div>
         </div>
-        </>
+        </>,
+        document.getElementById("loader-root")
+       )
     )
 }
 export default Loader
