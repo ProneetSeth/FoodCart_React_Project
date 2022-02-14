2eb73664d3c23c49918522c69e85955073518eaf
ProneetSeth <proneetofficial111@gmail.com>
2022-02-14 15:14:00 +0530

    Adding Cart Modal with static UI and rendering it conditionally

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,3 +1,5 @@
+import Cart from "../Cart"
+
 const Header = ({count}) => {
     return (
         <header>
@@ -37,18 +39,7 @@ const Header = ({count}) => {
                 </svg>
             </div>
             <div className="cart-container">
-                <button>
-                    <span className="cart" data-items={count}>Cart</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-search" height="30" viewBox="0 0 24 24"
-                             fill="none" strokeWidth="1.5" stroke="white" strokeLinejoin="round" strokeLinecap="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" /> 
-                            <circle cx="6" cy="19" r="2" />
-                            <circle cx="17" cy="19" r="2" />
-                            <path d="M17 17h-11v-14h-2" />
-                            <path d="M6 5l6.005 .429m7.138 6.573l-.143 .998h-13"/>
-                            <path d="M15 6h6m-3 -3v6"/> 
-                    </svg>
-                </button>
+                <Cart count={count}/>
             </div>
         </header>
     )
